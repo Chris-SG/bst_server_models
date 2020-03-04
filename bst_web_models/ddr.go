f077381cbb553727d730fe0839a6d75694825916
Chris-SG <christopher.shandgost@myob.com>
2020-03-04 17:24:32 +1100

    Change

--- bst_web_models/ddr.go
+++ bst_web_models/ddr.go
@@ -1,5 +1,7 @@
 package bst_web_models
 
+import "time"
+
 type DdrSongIds struct {
 	Ids []string `json:"ids"`
 }
@@ -7,4 +9,39 @@ type DdrSongIds struct {
 type DdrSongIdWithJacket struct {
 	Id string `json:"id"`
 	Jacket string `json:"jacket"`
+}
+
+type DdrScoresDetailedTopScore struct {
+	BestScore  int       `json:"score_record"`
+	Lamp       string    `json:"lamp"`
+	Rank       string    `json:"rank"`
+	PlayCount  int       `json:"playcount"`
+	ClearCount int       `json:"clearcount"`
+	MaxCombo   int       `json:"maxcombo"`
+	LastPlayed time.Time `json:"lastplayed"`
+
+	Mode       string `json:"mode"`
+	Difficulty string `json:"difficulty"`
+}
+
+type DdrScoresDetailedScore struct {
+	Score int `json:"score"`
+	ClearStatus bool `json:"cleared"`
+	TimePlayed time.Time `json:"timeplayed"`
+}
+
+type DdrScoresDetailedDifficulty struct {
+	Difficulty string `json:"difficulty"`
+	Scores []DdrScoresDetailedScore `json:"scores"`
+}
+
+type DdrScoresDetailedMode struct {
+	Mode string `json:"mode"`
+	Difficulties []DdrScoresDetailedDifficulty `json:"difficulties"`
+}
+
+type DdrScoresDetailed struct {
+	Id string `json:"id"`
+	TopScores []DdrScoresDetailedTopScore `json:"top_scores"`
+	Modes []DdrScoresDetailedMode `json:"modes"`
 }
